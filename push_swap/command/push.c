c4bace9da5ca265990cdefd5cc9ab4a634a5426d
能川瑛一貴 <eikinogawa@nogawaakirakazutakanoMacBook-Pro.local>
2022-10-21 14:14:12 +0900

    solved?

--- push_swap/command/push.c
+++ push_swap/command/push.c
@@ -6,11 +6,55 @@
 /*   By: enogawa <enogawa@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 11:49:55 by enogawa           #+#    #+#             */
-/*   Updated: 2022/10/20 16:27:50 by enogawa          ###   ########.fr       */
+/*   Updated: 2022/10/21 14:11:41 by enogawa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+void	node_sort_ope_pb(t_stacks **stack_a, t_stacks **stack_b)
+{
+	t_stacks	*head_a;
+	t_stacks	*node_a;
+	t_stacks	*head_b;
+	t_stacks	*node_b;
+	
+	if ((*stack_a)->data == -1)
+		*stack_a = (*stack_a)->next;
+	head_a =  list_mv_head(*stack_a);
+	node_a = head_a->prev;
+	node_b = *stack_b;
+	*stack_a = head_a->next;
+	head_a->next->prev = node_a;
+	node_a->next = head_a->next;
+	head_a->prev = node_b;
+	node_b->next = head_a;
+	node_b->prev = head_a;
+	head_a->next = node_b;
+	*stack_b = (*stack_b)->next;
+}
+
+void	node_sort_ope_pa(t_stacks **stack_a, t_stacks **stack_b)
+{
+	t_stacks	*head_a;
+	t_stacks	*node_a;
+	t_stacks	*head_b;
+	t_stacks	*node_b;
+
+	if ((*stack_b)->data == -1)
+		*stack_b = (*stack_b)->next;
+	head_b =  list_mv_head(*stack_b);
+	node_b = head_b->prev;
+	node_a = *stack_a;
+	*stack_b = head_b->next;
+	(*stack_b)->prev = node_b;
+	node_b->next = *stack_b;
+	head_b->prev = node_a;
+	node_a->next = head_b;
+	node_a->prev = head_b;
+	head_b->next = node_a;
+	*stack_a = (*stack_a)->next;
+}
 
 void	pa(t_stacks **stack_a, t_stacks **stack_b)
 {
@@ -24,28 +68,20 @@ void	pa(t_stacks **stack_a, t_stacks **stack_b)
 	head_b =  list_mv_head(*stack_b);
 	node_b = head_b->prev;
 	if ((*stack_a)->data == -1)
+		node_sort_ope_pa(stack_a, stack_b);
+	else
 	{
-		node_a = *stack_a;
+		head_a =  list_mv_head(*stack_a);
+		node_a = head_a->prev;
+		*stack_a = head_b;
 		*stack_b = head_b->next;
-		(*stack_b)->prev = node_b;
-		node_b->next = *stack_b;
+		head_b->next->prev = node_b;
+		node_b->next = head_b->next;
 		head_b->prev = node_a;
 		node_a->next = head_b;
-		node_a->prev = head_b;
-		head_b->next = node_a;
-		*stack_a = (*stack_a)->next;
+		head_b->next = head_a;
+		head_a->prev = head_b;
 	}
-	else
-	head_a =  list_mv_head(*stack_a);
-	node_a = head_a->prev;
-	*stack_a = head_b;
-	*stack_b = head_b->next;
-	head_b->next->prev = node_b;
-	node_b->next = head_b->next;
-	head_b->prev = node_a;
-	node_a->next = head_b;
-	head_b->next = head_a;
-	head_a->prev = head_b;
 	write(1, "pa\n", 3);
 }
 
@@ -61,17 +97,7 @@ void	pb(t_stacks **stack_a, t_stacks **stack_b)
 	head_a =  list_mv_head(*stack_a);
 	node_a = head_a->prev;
 	if ((*stack_b)->data == -1)
-	{
-		node_b = *stack_b;
-		*stack_a = head_a->next;
-		head_a->next->prev = node_a;
-		node_a->next = head_a->next;
-		head_a->prev = node_b;
-		node_b->next = head_a;
-		node_b->prev = head_a;
-		head_a->next = node_b;
-		*stack_b = (*stack_b)->next;
-	}
+		node_sort_ope_pb(stack_a, stack_b);
 	else
 	{
 		head_b =  list_mv_head(*stack_b);
@@ -87,36 +113,3 @@ void	pb(t_stacks **stack_a, t_stacks **stack_b)
 	}
 	write(1, "pb\n", 3);
 }
-
-
-
-void check_push(t_stacks *a, t_stacks *b)
-{
-	command_check(a);
-	command_check(b);
-	pb(&a, &b);
-	pb(&a, &b);
-	pb(&a, &b);
-	command_check(a);
-	command_check(b);
-	//printf("%ld", a->data);
-	pa(&a, &b);
-	pa(&a, &b);
-	// pa(&a, &b);
-	command_check(a);
-	command_check(b);
-	
-	// pb(&a, &b);
-	// pb(&a, &b);
-	// pb(&a, &b);
-	// command_check(a);
-	// command_check(b);
-	// pa(&a, &b);
-	// pa(&a, &b);
-	// pa(&a, &b);
-	// command_check(a);
-	// command_check(b);
-	
-}
-
-
--- push_swap/command/rotate.c
+++ push_swap/command/rotate.c
@@ -6,7 +6,7 @@
 /*   By: enogawa <enogawa@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 11:50:01 by enogawa           #+#    #+#             */
-/*   Updated: 2022/10/20 15:50:08 by enogawa          ###   ########.fr       */
+/*   Updated: 2022/10/21 14:13:24 by enogawa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,15 @@ void	ra(t_stacks **stack_a, int i)
 	if ((*stack_a)->data == -1)
 		*stack_a = (*stack_a)->next;
 	head =  list_mv_head(*stack_a);
-	//printf("\nhead; %ld\n", head->data);
 	node = head->prev;
-	//printf("head->next; %ld\n", head->next->data);
 	foot = list_mv_foot(*stack_a);
-	//printf("foot; %ld\n", foot->data);
-	
 	head->next->prev = node;
 	*stack_a = head->next;
 	head->next = node;
 	head->prev = foot;
 	foot->next = head;
-	
-	// while ((*stack_a)->data != -1)
-	// {
-	// 	printf("stack_a; %ld,", (*stack_a)->data);//test
-	// 	*stack_a = (*stack_a)->next;
-	// }
-
 	if (!i)
-	{
 		write (1, "ra\n", 4);
-	}
 }
 
 void	rb(t_stacks **stack_b, int i)
@@ -61,16 +48,8 @@ void	rb(t_stacks **stack_b, int i)
 	head->prev = foot->prev;
 	foot->prev = node;
 	node->next = foot;
-	
-	// head->next->prev = node;
-	// *stack_b = head->next;
-	// head->next = node;
-	// head->prev = foot;
-	// foot->next = head;
 	if (!i)
-	{
 		write (1, "rb\n", 4);
-	}
 }
 
 void	rr(t_stacks **stack_a, t_stacks **stack_b)
@@ -78,20 +57,4 @@ void	rr(t_stacks **stack_a, t_stacks **stack_b)
 	ra(stack_a, 1);
 	rb(stack_b, 1);
 	write (1, "rr\n", 4);
-}
-
-void	check_ra(t_stacks	*stack_a, int i)
-{
-	ra(&stack_a, i);
-	command_check(stack_a);
-	ra(&stack_a, i);
-	command_check(stack_a);
-	ra(&stack_a, i);
-	command_check(stack_a);
-	ra(&stack_a, i);
-	command_check(stack_a);
-	ra(&stack_a, i);
-	command_check(stack_a);
-	ra(&stack_a, i);
-	command_check(stack_a);
 }
