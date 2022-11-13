912c8e5a9df539a6002eec4b6000bc344e416d96
能川瑛一貴 <eikinogawa@nogawaakirakazutakanoMacBook-Pro.local>
2022-11-13 21:16:42 +0900

    almost

---
+++ push_swap/list_ope/list_add_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_add_back.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enogawa <enogawa@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 20:59:34 by enogawa           #+#    #+#             */
+/*   Updated: 2022/11/13 20:59:35 by enogawa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	lst_add_back(t_stacks *stacks, t_stacks *add)
+{
+	t_stacks	*node;
+
+	if (!stacks || !add)
+		return ;
+	stacks->next = add;
+	add->prev = stacks;
+	node = list_mv_head(stacks);
+	node = node->prev;
+	//printf("add; %ld, ", node->data);
+	add->next = node;
+	node->prev = add;//tmp
+}
